020276cc182191a5c5af7dd5cf38adca6558cde0
Sergio Toro <yiti007@gmail.com>
2021-01-10 22:02:35 +0100

    remove from constants

--- renderer/utils/constants.js
+++ renderer/utils/constants.js
@@ -1,2 +1,2 @@
-export const MAP_TILER_KEY = process.env.NEXT_PUBLIC_MAP_TILER_KEY || 'YQEXUOerAO91Fi6w7t2H';
-export const MAP_TILER_GEO_KEY = process.env.NEXT_PUBLIC_MAP_TILER_GEO_KEY || 'ONrAq6OSzf2DOFQLuqxr';
+export const MAP_TILER_KEY = process.env.NEXT_PUBLIC_MAP_TILER_KEY;
+export const MAP_TILER_GEO_KEY = process.env.NEXT_PUBLIC_MAP_TILER_GEO_KEY;
