22b4492d5ce9a55e55dff8a7a093616aebee02e9
Sergio Toro <yiti007@gmail.com>
2021-01-10 10:55:39 +0100

    add dark/light theme

--- renderer/pages/_app.jsx
+++ renderer/pages/_app.jsx
@@ -1,10 +1,36 @@
-
-import '../styles.css';
+import { useMemo } from 'react';
+import Head from 'next/head';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 
 function MyApp({ Component, pageProps }) {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
   return (
-    <Component {...pageProps} />
+    <>
+      <Head>
+        <meta
+          key="viewport"
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
